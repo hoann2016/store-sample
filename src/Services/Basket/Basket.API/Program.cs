ab9edd2346052c73add9018d45278a6f8ab8d61e
Hoa Nguyen <hoann.2016@gmail.com>
2024-05-15 15:42:12 +0700

    Add support docker, redish for basket

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -3,7 +3,10 @@ using Basket.API.Models;
 using BuildingBlocks.Behaviors;
 using BuildingBlocks.Exceptions.Handler;
 using Carter;
+using HealthChecks.UI.Client;
 using Marten;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Caching.Distributed;
 using Weasel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,11 +26,32 @@ builder.Services.AddMarten(option =>
     })
     .UseLightweightSessions();
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+builder.Services.AddStackExchangeRedisCache(option =>
+{
+    option.Configuration = builder.Configuration.GetConnectionString("Redis")!;
+   // option.InstanceName = "Basket";
+});
+//builder.Services.AddScoped(provider =>
+//{
+//    var basketRepo= provider.GetRequiredService<IBasketRepository>();
+//    return new CachedBasketRepository(basketRepo, provider.GetRequiredService<IDistributedCache>());
+//});
+
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
 
 app.MapCarter();
 app.UseExceptionHandler(option=>{});
+app.UseHealthChecks("/health",
+    new HealthCheckOptions()
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 app.Run();
